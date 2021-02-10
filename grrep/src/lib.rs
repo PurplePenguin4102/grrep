077d4e28f1997d5a0d846e456a3579763ce3b2a3
Joey <joseph.ray@workpac.com>
2021-02-10 05:55:37 -0800

    final final

--- grrep/src/lib.rs
+++ grrep/src/lib.rs
@@ -75,9 +75,9 @@ mod tests {
     fn one_result() {
         let query = "duct";
         let contents = "\
-  Rust:
-  safe, fast, productive.
-  Pick three.";
+Rust:
+safe, fast, productive.
+Pick three.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
@@ -86,10 +86,10 @@ mod tests {
     fn case_insensitive() {
         let query = "rUsT";
         let contents = "\
-  Rust:
-  safe, fast, productive.
-  Pick three.
-  Trust me.";
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
 
         assert_eq!(
             vec!["Rust:", "Trust me."],
