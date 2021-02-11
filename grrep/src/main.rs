d25900e678791a17ec920ff39089d15ffe3201bd
Joey <joseph.ray@workpac.com>
2021-02-10 19:36:00 -0800

    dekita

--- grrep/src/main.rs
+++ grrep/src/main.rs
@@ -5,15 +5,12 @@ use std::error::Error;
 use grrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    //let cfg = parse_config(&args);
-    let cfg = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
-    if let Err(e) = grrep::run(cfg) {
+    if let Err(e) = grrep::run(config) {
         eprintln!("Application error: {}", e);
         process::exit(1);
     }
