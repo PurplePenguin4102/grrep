ed923c143c3a80b7b4f701b5b4981e29c0428604
Joey <joseph.ray@workpac.com>
2021-02-10 19:42:15 -0800

    thunk

--- grrep/src/lib.rs
+++ grrep/src/lib.rs
@@ -60,6 +60,15 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
+/*
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
+}
+*/
+
 pub fn search_case_insensitive<'a>(
     query: &str,
     contents: &'a str) 
